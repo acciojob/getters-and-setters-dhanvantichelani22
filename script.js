c6d9bc2248b37a3d5c36252b4511e11d73126d58
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-05-18 10:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ class Person {
     this.name = nameVal;
     this.age = ageVal;
 }
-get getName() {
+get nameVal() {
     return this.name;
   }
 
-  set setAge(ageVal) {
+  set ageVal(ageVal) {
     // this.age = ageVal;
 	  return "Age: " + this.age;
   }
 }
 
 var user1 = new User("Moghul", 21);
-console.log(user1.getName);
-console.log(user1.setAge);
+console.log(user1.nameVal);
+console.log(user1.ageVal);
 class Student extends Person {
 	study() {
     console.log(this.name+"is studying");
